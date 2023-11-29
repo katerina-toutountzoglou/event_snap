ee2fabce8e6d4c8a5d60e42fb2d186b7768b17b6
katerina <a_tou_toun@hotmail.com>
2023-11-29 15:10:28 +0200

    cities events

--- routes/citiesRoute.js
+++ routes/citiesRoute.js
@@ -6,6 +6,28 @@ import City from '../models/city.js';
 
 router.get('/getallcities',async (req,res)=>{
     try{
+        let query={};
+        
+        
+        // Check if name parameter is provided
+        if (req.query.name) {
+           query.name = req.query.name;
+        }
+                
+        //console.log(query);
+        const cities = await City.find(query);
+        res.json(cities);
+        
+    }catch(error){
+        return res.status(500).json({message: error});
+
+    }
+});
+
+
+/*
+router.get('/getallcities',async (req,res)=>{
+    try{
         const cities=await City.find({});
        res.send(cities);
 
@@ -46,6 +68,6 @@ router.post('/getcitybyname/:cityname',async (req,res)=>{
 });
 
 
-
+*/
 
 export default router;
--- routes/eventsRoute.js
+++ routes/eventsRoute.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
 
 const router= express.Router();
 import Event from '../models/event.js';
@@ -56,20 +58,6 @@ router.post('/geteventbyid',async (req,res)=>{
 });
 
 /*
-router.post('/geteventbylocation',async (req,res)=>{
-    
-    const eventlocation=req.body.eventlocation
-    try{
-        const event=await Event.find({location: eventlocation});
-       res.send(event);
-       console.log(event);
-
-    }catch(error){
-        return res.status(400).json({message: error});
-
-    }
-});
-*/
 router.get('/geteventbylocation/:eventlocation',async (req,res)=>{
     try{
         const {eventlocation}=req.params;
@@ -82,36 +70,10 @@ router.get('/geteventbylocation/:eventlocation',async (req,res)=>{
 
     }
 });
-/*
-router.patch('/bookevent',async (req,res)=>{
-   
-
-    //...
-    try{
-         const {eventid,numoftickets}=req.body.bookevent;
-        
-      
-      const event=await Event.findOne({_id: eventid})
-       if(!event){
-        return res.status(400).json({message: error});
-       }
-       if(event.remaining_tickets<numoftickets){
-        return res.status(400).json({message:'Sorry! Not enough tickets!'});
-       }
-       const temp= event.remaining_tickets-numoftickets;
-       event.remaining_tickets=temp;
-
-       const event2=await Event.findOneAndUpdate({_id: eventid},{remaining_tickets:temp},{new:true});
-
-       res.send({message: 'You have made a successful booking!'});
-       
-    }catch(error){
-        return res.status(400).json({message: error});
-
-    }
-});
 */
-//
+/*
+
+
 router.patch('/bookevent/:eventid/:numoftickets',async (req,res)=>{
    
     try{
@@ -141,6 +103,7 @@ router.patch('/bookevent/:eventid/:numoftickets',async (req,res)=>{
     }
 });
 
+*/
 
 
 export default router;
